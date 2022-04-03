35223bbae2e4c6a935a56aedbe11d38f733c2456
OskarStefanski <stefek73@gmail.com>
2022-04-03 18:26:30 +0200

    metoda Get Letter Grade testy 9/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,8 +8,25 @@ namespace GradeBook.GradeBooks
 
     {
         public RankedGradeBook(string name) : base(name)
-        {
+         {
             Type = Enums.GradeBookType.Ranked;
+         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            else if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+
         }
     }
 }

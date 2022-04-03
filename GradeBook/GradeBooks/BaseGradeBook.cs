63de01b0245805d5bfb249394842fb01fae3e809
OskarStefanski <stefek73@gmail.com>
2022-04-03 23:04:49 +0200

    19/24 aktualizacja w helpcommand

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -113,20 +113,27 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int bonus = (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) ? 1 : 0;
             switch (letterGrade)
             {
                 case 'A':
                     return 4;
+                    return 4 + bonus;
                 case 'B':
                     return 3;
+                    return 3 + bonus;
                 case 'C':
                     return 2;
+                    return 2 + bonus;
                 case 'D':
                     return 1;
+                    return 1 + bonus;
                 case 'F':
                     return 0;
+                    return 0 + bonus;
             }
             return 0;
+            return 0 + bonus;
         }
 
         public virtual void CalculateStatistics()
